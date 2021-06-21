fd7f6f1b79226a9d28f80337b35e61a366c6c63a
Blake Derbin <blake.derbin@hotmail.com>
2021-06-21 12:37:32 +1000

    v0.2.1 Mobile responsiveness: crypto-performers component

--- src/app/components-shared/crypto/crypto-performers/crypto-performers.component.ts
+++ src/app/components-shared/crypto/crypto-performers/crypto-performers.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable, Subscribable, Subscription} from "rxjs";
+import {Component, Directive, OnInit} from '@angular/core';
+import {Observable, Subject, Subscribable, Subscription} from "rxjs";
 import IcryptoMarket, {ImarketData} from "../../../components-services/crypto/ImarketData.model";
 import {CryptoControllerService} from "../../../components-services/crypto/crypto-controller.service";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {takeUntil} from "rxjs/operators";
+
+interface MediaQueryList extends EventTarget {
+  matches: boolean; // => true if document matches the passed media query, false if not
+  media: string; // => the media query used for the matching
+}
+
+
 
 @Component({
   selector: 'app-crypto-performers',
@@ -13,10 +22,73 @@ export class CryptoPerformersComponent implements OnInit {
   subscription: Subscription;
   cryptoData = new Array<IcryptoMarket>();
 
-  constructor(public service: CryptoControllerService) { }
+  //**
+  destroyed = new Subject<void>();
+  currentScreenSize: string = 'Large';
+
+  //**
+  // create a map to display breakpoint names for demo
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'xSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+  ])
+
+  viewMap = new Map([
+    [Breakpoints.Handset, 'mobile'],
+    [Breakpoints.Web, 'desktop']
+  ])
+
+  isSmallScreen = this.breakpointObserver.isMatched('(max-width: 599px');
+
+  //**
+  constructor(public service: CryptoControllerService, public breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+    ]).pipe(takeUntil(this.destroyed)).subscribe(result => {
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.getTop3();
+  }
+
+  //**
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
+  //switch between mobile and desktop views
+  switchViewMode() {
+    const query = '(orientation: portrait)';
+    const mediaQueryList = window.matchMedia(query);
+
+    // define the callback function for our event listener
+    function listener(mqs: MediaQueryList) {
+      // checks the match
+      if(mqs.matches) {
+        console.log("portrait mode")
+      } else {
+        console.log("landscape mode")
+      }
+    }
+
+    // run check once
+    listener(mediaQueryList);
+
+    // run check on every subsequent change
+    // @ts-ignore
+    mediaQueryList.addEventListener('change', listener);
   }
 
   //using observable get data from api
